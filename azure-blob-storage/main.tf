6899f5867cc4d8d3e7f91c3e3f07e387134e5896
oskar2k20 <103363025+oskar2k20@users.noreply.github.com>
2022-05-23 19:42:35 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,33 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.name_example_resources
+  location = var.location
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.name_storage_account
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
+
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.name_storage_container
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = var.access_container_type
+}
+
+
+resource "azurerm_storage_blob" "example" {
+  name                   = var.name_storage_blob
+  url                    = var.url_storage_blob
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = var.type_storage_blob
+  source                 = var.source_storage_blob
+}
+
+
 
